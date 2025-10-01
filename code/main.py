dd752191035dbd40ae6bb0893f7cc90dc5b10c95
Coderdojo <zotcoderdojo@gmail.com>
2025-10-01 13:33:29 +0200

    doc en commentaar

--- code/main.py
+++ code/main.py
@@ -1,13 +1,23 @@
+####################################################################################
+# Dit is de start van een main file
+#
+# Laatste update: 
+####################################################################################
 from pybricks.hubs import PrimeHub
 from pybricks.pupdevices import Motor, ColorSensor, UltrasonicSensor, ForceSensor
 from pybricks.parameters import Button, Color, Direction, Port, Side, Stop
 from pybricks.robotics import DriveBase
 from pybricks.tools import wait, StopWatch
+
 hub = PrimeHub()
 start_stop = 0
 programma = 0
 pressed = []
 getal = 1
+
+# de code van de buttons gebruiken kan hier blijven staan als we de besturing
+# via buttons doen, anders misschien beter in een aparte .py file?
+
 while True:
     pressed = []
     while not any(pressed):
