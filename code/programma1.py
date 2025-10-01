93af3b054e0d1f52161d09f86fea080cfd6ec4f6
Coderdojo <zotcoderdojo@gmail.com>
2025-10-01 13:52:05 +0200

    Update programma1.py

--- code/programma1.py
+++ code/programma1.py
@@ -1,10 +1,18 @@
+####################################################################################
+# Deze file laat zien hoe je een functie geschreven in een andere python file kan gebruiken
+# 
+####################################################################################
 from pybricks.hubs import PrimeHub
 from pybricks.pupdevices import Motor, ColorSensor, UltrasonicSensor
 from pybricks.parameters import Button, Color, Direction, Port, Side, Stop
 from pybricks.robotics import DriveBase
 from pybricks.tools import wait, StopWatch
+
+# Hier doen we de import
 from rijden_met_gyro_versie_lukas import gyro_rechtdoor
+
+# niet zeker of dit nodig is, want zit ook al in de import
 hub = PrimeHub
 
-
+# op deze manier kunnen we de functie gebruiken
 gyro_rechtdoor(500, 1000)
