4a05fae90cffa20b446d25806e1aa3e2eb50cc6f
Coderdojo <zotcoderdojo@gmail.com>
2025-10-18 16:25:27 +0200

    Create missie_geel.py

---
+++ 2025/missie_geel.py
@@ -0,0 +1,10 @@
+from functies import *
+
+# in deze functie komt alle code voor de missie
+def missie_geel_start():
+   vooruit(100)
+
+
+# laat dit zeker staan!
+if __name__ == "__main__":
+    missie_geel_start()
