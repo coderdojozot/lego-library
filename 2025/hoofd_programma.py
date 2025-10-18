e2837d13d1875eba36fdc6b23cdb12753514c1bc
Coderdojo <zotcoderdojo@gmail.com>
2025-10-18 18:29:17 +0200

    Update hoofd_programma.py

--- 2025/hoofd_programma.py
+++ 2025/hoofd_programma.py
@@ -1,16 +1,38 @@
 from functies import *
 from missie_blauw import missie_blauw_start
 from missie_groen import missie_groen_start
+from missie_geel import missie_geel_start
 laatste_kleur = Color.MAGENTA
 
 while True:
     color =sensor.color()
-    print(color)
     if color == laatste_kleur or color == Color.NONE:
         continue
     laatste_kleur = color    
     if color == Color.GREEN: 
-        wait(500)    
-        missie_groen_start()
+        hub.light.blink(color,[500,500])
+        selected = hub_menu("A", "F")  
+        if selected == "A":
+             hub.light.on(color)
+             missie_groen_start()
+             hub.light.off()
+             hub.display.off()
+              
+        
     if color == Color.BLUE:
-        missie_blauw_start()
+        hub.light.blink(color,[500,500])
+        selected = hub_menu("A", "F")  
+        if selected == "A":
+             hub.light.on(color)
+             missie_blauw_start()
+             hub.light.off()
+             hub.display.off()
+
+    if color == Color.YELLOW:
+        hub.light.blink(color,[500,500])
+        selected = hub_menu("A", "F")  
+        if selected == "A":
+             hub.light.on(color)
+             missie_geel_start()
+             hub.light.off()
+             hub.display.off()
