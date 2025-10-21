6291ae2886d14e527e21436aa72a62d0e6f6c5c1
Coderdojo <zotcoderdojo@gmail.com>
2025-10-21 19:08:35 +0200

    je moet allen in de task .json het pad aanpassen staat ook in het bestand waar je het moet aanpassen en druk op Ctrl+shift+B om het progamma te runnen

--- code/start_code.py
+++ code/start_code.py
@@ -1,17 +1,3 @@
-from pybricks.hubs import PrimeHub
-from pybricks.parameters import Color
-from pybricks.tools import wait
+from basecode import *
 
-# Initialize hub
-hub = PrimeHub()
-
-# Test sequence with multiple colors
-print("Starting connection test...")
-
-# Try different colors to make sure we're connected
-for color in [Color.RED, Color.GREEN, Color.BLUE]:
-    print(f"Setting light to {color}")
-    hub.light.on(color)
-    wait(1000)
-
-print("Connection test completed!")
+basecode.GyroDrive(500)
