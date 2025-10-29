bf978f7bbc2a752827f0801326f9f81caf725cbc
Coderdojo <zotcoderdojo@gmail.com>
2025-10-29 16:06:54 +0100

    Geel B, missie 11

---
+++ 2025/missie_geelB.py
@@ -0,0 +1,14 @@
+from functies import *
+
+async def missie_geelB_start():
+    await draai_rechts_met_straal(90,300)
+    await vooruit(578)
+    await rechts(90)
+    await vooruit(150)
+    await links(15)
+    await rechterarm_draai(2400)
+    await achteruit(150)
+    await links(90)
+
+if __name__ == "__main__": 
+    run_task(missie_geelB_start())
