284bd81b127e5219b09efcf6ee678cde16a9dca4
Coderdojo <zotcoderdojo@gmail.com>
2025-10-29 16:28:31 +0100

    missie groenA en groenB werken via hoofdprogramma

--- 2025/hoofd_programma.py
+++ 2025/hoofd_programma.py
@@ -1,6 +1,7 @@
 from functies import *
 from missie_blauw import missie_blauw_start
-from missie_groenA import missie_groen_start
+from missie_groenA import missie_groenA_start
+from missie_groenB import missie_groenB_start
 from missie_geel import missie_geel_start
 laatste_kleur = Color.MAGENTA
 
@@ -11,12 +12,17 @@ while True:
     laatste_kleur = color    
     if color == Color.GREEN: 
         hub.light.blink(color,[500,500])
-        selected = hub_menu("A", "F")  
+        selected = hub_menu("A", "B")  
         if selected == "A":
              hub.light.on(color)
-             run_task(missie_groen_start())
+             run_task(missie_groenA_start())
              hub.light.off()
              hub.display.off()
+        if selected == "B":
+            hub.light.on(color)
+            run_task(missie_groenB_start())
+            hub.light.off()
+            hub.display.off()
               
         
     if color == Color.BLUE:
