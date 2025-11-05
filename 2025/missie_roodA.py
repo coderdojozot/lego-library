2e40833aadd4e1a6763c84beb7e6aee6cd03907e
Coderdojo <zotcoderdojo@gmail.com>
2025-11-05 13:17:07 +0100

    start voor missie 9, 10 roodA stuk
    Rood A stuk nog te maken

---
+++ 2025/missie_roodA.py
@@ -0,0 +1,16 @@
+from functies import *
+
+# Arthus
+# dit is voor missie 9 - 10 
+
+async def missie_roodA_start():
+  # Dit is voorbeeldcode voor de start van een nieuw
+  # programma. Pas deze code aan voor jouw extensie.
+  
+  # await vooruit(100)
+  # await multitask(vooruit(2),rechterarm.run_angle(900,-1650))
+
+# Deze regel zorgt ervoor dat de code uitgevoerd wordt
+# verander enkel de naam van jouw missie, maar laat de code verder zoals ze is
+if __name__ == "__main__":
+    run_task(missie_roodA_start())
