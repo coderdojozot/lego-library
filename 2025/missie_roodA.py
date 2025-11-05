60a0ac5aa9ab5a472777adb32dc56171293a2d4f
Coderdojo <zotcoderdojo@gmail.com>
2025-11-05 13:19:10 +0100

    Update missie_roodA.py

--- 2025/missie_roodA.py
+++ 2025/missie_roodA.py
@@ -8,9 +8,13 @@ async def missie_roodA_start():
   # programma. Pas deze code aan voor jouw extensie.
   
   # await vooruit(100)
+  # await rechts(85)
+  # await links(8)
+  # await rechterarm.run_angle(2100,1650)
   # await multitask(vooruit(2),rechterarm.run_angle(900,-1650))
 
 # Deze regel zorgt ervoor dat de code uitgevoerd wordt
 # verander enkel de naam van jouw missie, maar laat de code verder zoals ze is
 if __name__ == "__main__":
     run_task(missie_roodA_start())
+
