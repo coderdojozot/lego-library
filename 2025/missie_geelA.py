3291e4423420182122856f4a80e35dc848e8cfb0
Coderdojo <zotcoderdojo@gmail.com>
2025-11-12 15:28:44 +0100

    Update final

--- 2025/missie_geelA.py
+++ 2025/missie_geelA.py
@@ -2,20 +2,22 @@ from functies import *
 
 # Luca
 # dit is voor missie 3, 9, 10 
+# startveld link, 2de zwart streepje van links
 async def missie_geelA_start():
    await vooruit(695)
    await rechts(85)
    await vooruit(362)
    await links(78)
-   await multitask(vooruit(4),rechterarm.run_angle(900,-1670))
-   await rechterarm.run_angle(2100,1670)
+   await multitask(vooruit(4),rechterarm.run_angle(900,-1670))    # Heffen mijnkarretje
+   await rechterarm.run_angle(2100,1670)                          # Arm omlaag
    await multitask(vooruit(2),rechts(70))
-   await multitask(vooruit(625),rechterarm.run_angle(900,-1670))
+   await multitask(vooruit(625),rechterarm.run_angle(900,-1670))  # Arm omhoog
    await rechts(89)
-   await rechterarm.run_angle(900,1670)
+   await rechterarm.run_angle(900,1670)                           # Weegschaal omlaag
    await achteruit(35)
-   await multitask(rechterarm.run_angle(900,-150),links(29.5))
-   await vooruit(302,900)
+   await multitask(rechterarm.run_angle(900,-150),links(29.375))    # Arm omhoog
+   await vooruit(303,900)                                         # Omhoogduwen winkelding
+   await wait(10)
    await achteruit(220)
    await links(10)
    await vooruit(450)
--- 2025/missie_roodB.py
+++ 2025/missie_roodB.py
@@ -3,6 +3,10 @@ from functies import *
 # Dit is voorbeeldcode voor de start van een nieuw
 # programma. Pas deze code aan voor jouw extensie.
 
+
+# Robi, code is klaar.
+# Startveld rechts, 2de zwart streepje van rechts
+
 async def missie_roodB_start():
      await vooruit(425)
      await multitask(linkerarm.run_angle(900,-168),rechterarm.run_angle(900,-168))
@@ -12,7 +16,9 @@ async def missie_roodB_start():
      await multitask(linkerarm.run_angle(900,-168),rechterarm.run_angle(900,-168))
      await multitask(linkerarm.run_angle(900,168),rechterarm.run_angle(900,168))
      await vooruit(-418)
+     
 # Deze regel zorgt ervoor dat de code uitgevoerd wordt
 # verander enkel de naam van jouw missie, maar laat de code verder zoals ze is
+
 if __name__ == "__main__":
     run_task(missie_roodB_start())
