3236b5f9c66870a1b1487983c8f192ea963391ec
Nath Nacht <43142008+NathNacht@users.noreply.github.com>
2025-11-19 15:20:26 +0100

    ldfibjdfiubhksdgbhsdfsviouqbfousdnvlishbsdxjvsudjg

--- 2025/hoofd_programma.py
+++ 2025/hoofd_programma.py
@@ -1,7 +1,6 @@
 from functies import *
 from missie_blauw import missie_blauw_start
 from missie_groenA import missie_groenA_start
-from missie_groenB import missie_groenB_start
 from missie_geelA import missie_geelA_start
 from missie_geelB import missie_geelB_start
 from missie_roodA import missie_roodA_start
@@ -13,8 +12,14 @@ from missie_roodB import missie_roodB_start
 
 laatste_kleur = Color.MAGENTA
 
+paars = [4 ,5 ,6]
+groen = [1 ,2 ,3]
+geel = [17, 18, 19]
+orange = [8 ,9 ,10]
+blauw = [1, 2, 3]
+
 while True:
-    color =sensor.color()
+    color = sensor.color()
     if color == laatste_kleur or color == Color.NONE:
         continue
     laatste_kleur = color    
@@ -26,11 +31,6 @@ while True:
              run_task(missie_groenA_start())
              hub.light.off()
              hub.display.off()
-        if selected == "B":
-            hub.light.on(color)
-            run_task(missie_groenB_start())
-            hub.light.off()
-            hub.display.off()
               
         
     if color == Color.BLUE:
