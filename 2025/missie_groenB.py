0c4e66e1adbadb3e32082b29a29aebd6a277d2f4
coderdojozot <zotcoderdojo@gmail.com>
2025-11-22 10:02:23 +0100

    groenB eerste versie

---
+++ 2025/missie_groenB.py
@@ -0,0 +1,19 @@
+from functies import *
+
+# [naam]
+# dit is voor missie [] 
+
+async def missie_[groenB]_start():
+  # Dit is voorbeeldcode voor de start van een nieuw
+  # programma. Pas deze code aan voor jouw extensie.
+  
+  # await vooruit(100)
+  # await rechts(85)
+  # await links(8)
+  # await rechterarm.run_angle(2100,1650)
+  # await multitask(vooruit(2),rechterarm.run_angle(900,-1650))
+
+# Deze regel zorgt ervoor dat de code uitgevoerd wordt
+# verander enkel de naam van jouw missie, maar laat de code verder zoals ze is
+if __name__ == "__main__":
+    run_task(missie_[groenB]_start())
