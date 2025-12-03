3c8bee1e2f7ecd2c0ba8e5f0e7a1293266fbc2a2
coderdojozot <zotcoderdojo@gmail.com>
2025-12-03 15:36:42 +0100

    kjfgksdgf

--- 2025/missie_roodB.py
+++ 2025/missie_roodB.py
@@ -2,32 +2,39 @@ from functies import *
 
 """
 ---------------
-BOUWERS: Robi
+BOUWERS: Robi & Lukas
 MISSIE(S): 8 (silo muur) - 13 (dino standbeeld)
 PUNTEN: 10x3, 30
 STARTPOSITIE: Rechter startveld, 2de rechtse zwarte streepje, rechterkant robot
 
-STATUS: code voor standbeeld nog aanpassen
-TESTDATUM/ROBOT: 26/11/2025 - hub1
+STATUS: 
+TESTDATUM/ROBOT: 3/12/2025 - hub1
 ---------------
 """
+frequentie = 50
+duur = 300
 
 async def missie_roodB_start():
      await achteruit(15)
      await vooruit(435)
-     await multitask(linkerarm.run_angle(1000,-168),rechterarm.run_angle(1000,-168))
-     await multitask(linkerarm.run_angle(1000,168),rechterarm.run_angle(1000,168))
-     await multitask(linkerarm.run_angle(1000,-168),rechterarm.run_angle(1000,-168))
-     await multitask(linkerarm.run_angle(1000,168),rechterarm.run_angle(1000,168))
-     await multitask(linkerarm.run_angle(1000,-168),rechterarm.run_angle(1000,-168))
-     await multitask(linkerarm.run_angle(1000,168),rechterarm.run_angle(1000,168))
-     await vooruit(-100)
+     await multitask(linkerarm.run_angle(1000,-200),rechterarm.run_angle(1000,-200))  #tandwielenc uit silo 
+     await hub.speaker.beep(frequentie, duur)
+     await multitask(linkerarm.run_angle(1000,200),rechterarm.run_angle(1000,200))
+     await multitask(linkerarm.run_angle(1000,-200),rechterarm.run_angle(1000,-200))
+     await hub.speaker.beep(frequentie, duur)
+     await multitask(linkerarm.run_angle(1000,200),rechterarm.run_angle(1000,200))
+     await multitask(linkerarm.run_angle(1000,-200),rechterarm.run_angle(1000,-200))
+     await hub.speaker.beep(frequentie, duur)
+     await multitask(linkerarm.run_angle(1000,200),rechterarm.run_angle(1000,200))      #done
+     await vooruit(-100)                                 #rijden naar dino skelet
      await draai_links_met_straal(90,400)
      await vooruit(296)
-     await links(45)
-     await vooruit(185)
-     await multitask(linkerarm.run_angle(1000,-168),rechterarm.run_angle(1000,-168))
-     await multitask(linkerarm.run_angle(1000,168),rechterarm.run_angle(1000,168)) 
+     await links(44)
+     await vooruit(178)                          #done
+     await multitask(linkerarm.run_angle(1000,-240),rechterarm.run_angle(1000,-250)) #dino skelet omhoog
+     rechterarm.hold()    
+     await vooruit(40)
+     #await multitask(linkerarm.run_angle(1000,240),rechterarm.run_angle(1000,250))       #done
 
 if __name__ == "__main__":
     run_task(missie_roodB_start())
