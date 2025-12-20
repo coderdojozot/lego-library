0bccd9e425f487c0de86061487aca45512e64aa3
coderdojozot <zotcoderdojo@gmail.com>
2025-12-20 11:14:42 +0100

    missie geel B laatste versie

--- 2025/missie_geelB.py
+++ 2025/missie_geelB.py
@@ -14,13 +14,14 @@ TESTDATUM/ROBOT: 26/11/2025 - hub1
 
 async def missie_geelB_start():
     await draai_rechts_met_straal(90,300)
-    await vooruit(578)
+    await vooruit(598)
     await rechts(90)
     await vooruit(150)
-    await links(15)
+    await links(20)
     await rechterarm_draai(2400)
-    await achteruit(150)
-    await links(90)
+    await achteruit(300)
+    await rechts(110)
+    await vooruit(800)
 
 if __name__ == "__main__": 
     run_task(missie_geelB_start())
