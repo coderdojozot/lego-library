8470634938b83c4dc39d3050ced5ab0507dda57b
Coderdojo <zotcoderdojo@gmail.com>
2025-12-20 11:33:38 +0100

    blauw B complete

--- 2025/missie_blauwB.py
+++ 2025/missie_blauwB.py
@@ -20,12 +20,14 @@ async def missie_blauwB_start():
 
   await multitask(vooruit(750),linkerarm_draai(1500))
   await rechterarm_draai(270)
-  await rechts(51)
+  await rechts(53)
   await achteruit(25)
-  await linkerarm_draai(450)
+  await linkerarm_draai(455)
   await vooruit(65) 
   await linkerarm_draai(-1200)
   await links(150)
-
+  wait(1000)
+  await links(90)
+  await vooruit(2000)
 if __name__ == "__main__":
     run_task(missie_blauwB_start())
