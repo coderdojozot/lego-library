9c074d61814458ace89462c8ed15386f9a8dc696
Coderdojo <zotcoderdojo@gmail.com>
2026-01-17 11:01:12 +0100

    geelA coderdojo zaterdag

--- 2025/missie_geelA.py
+++ 2025/missie_geelA.py
@@ -11,31 +11,30 @@ STARTPOSITIE: 2de linkse zwarte streepje, linkerkant robot
 
 STATUS: werkt volledig
 TESTDATUM/ROBOT: 26/11/2025 - hub1
-BATTERIJ % BIJ START: xxx
+BATTERIJ % BIJ START: 99.01%
 ---------------
 """
 
 async def missie_geelA_start():
-   await vooruit(735)
+   await vooruit(733.5)
    await rechts(85)
    await vooruit(362)
    await links(78)
-   await multitask(vooruit(4),rechterarm.run_angle(900,-3000))    # Heffen mijnkarretje
+   await multitask(vooruit(1.85),rechterarm.run_angle(900,-2000))    # Heffen mijnkarretje
    await multitask(vooruit(2),rechts(70))
    await vooruit(625)  # Arm omhoog
    await rechts(89)
    await vooruit(50)
-   await rechterarm.run_angle(900,1670) 
-   await achteruit(50)                        # Weegschaal omlaag
-   await achteruit(35)
-   await multitask(rechterarm.run_angle(900,-150),links(35))    # Arm omhoog
-   await vooruit(270, 900)                                      # Omhoogduwen winkelding
+   await rechterarm.run_angle(900,1695)                         
+   await achteruit(26)                                      # Weegschaal omlaag
+   await multitask(rechterarm.run_angle(900,-250),links(31.35))   # Arm omhoog
+   await vooruit(228, 900)                                      # Omhoogduwen winkelding
    await wait(10)
    await achteruit(220)
-   await links(10)
+   await links(18)
    await vooruit(450)
    await rechts(45)
-   await multitask(rechterarm.run_angle(900,150),vooruit(450))
+   await multitask(rechterarm.run_angle(900,250),vooruit(450))
    
    
 # Deze regel zorgt ervoor dat de code uitgevoerd wordt
