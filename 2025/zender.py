82053ff127f4c42aefde0cb33100edaa6a23b26d
Coderdojo <zotcoderdojo@gmail.com>
2026-01-24 11:43:14 +0100

    goed

---
+++ 2025/zender.py
@@ -0,0 +1,12 @@
+from pybricks.hubs import PrimeHub
+from pybricks.pupdevices import Motor
+from pybricks.parameters import Port
+from pybricks.tools import wait
+
+# Initialize the hub.
+hub = PrimeHub(broadcast_channel=1)
+
+
+bricht = "hallo"
+
+hub.ble.broadcast(bricht)
