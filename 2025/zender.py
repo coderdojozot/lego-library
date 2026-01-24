b13734e35b063274b27db71a5896b4d03a4b9bb4
coderdojozot <zotcoderdojo@gmail.com>
2026-01-24 12:12:19 +0100

    zender

--- 2025/zender.py
+++ 2025/zender.py
@@ -1,13 +1,17 @@
 from pybricks.hubs import PrimeHub
-from pybricks.pupdevices import Motor
-from pybricks.parameters import Port
+from pybricks.pupdevices import Motor, ColorSensor
+from pybricks.parameters import Port, Color
 from pybricks.tools import wait
 
 # Initialize the hub.
 hub = PrimeHub(broadcast_channel=1)
-sensor1 = 
-
-bricht = "hallo"
+sensor1 = ColorSensor(Port.D)
 
 while True:
-    hub.ble.broadcast(bricht)
+    if sensor1.color() == Color.BLUE:
+        hub.light.on(Color.BLUE)
+        hub.ble.broadcast("run")
+    else:
+        hub.light.off()
+        hub.ble.broadcast("stop")
+
